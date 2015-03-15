0c7a78029fbb90626408b36572ea5912c57bc498
Sölvi Logason <logason@gmail.com>
2015-03-15 01:37:20 +0000

    enter transition for flights in my journey

--- scripts/components/JourneyPlan.jsx
+++ scripts/components/JourneyPlan.jsx
@@ -3,6 +3,7 @@
 import _ from 'lodash';
 import moment from 'moment';
 import React from 'react/addons';
+import TimeoutTransitionGroup from '../TimeoutTransitionGroup';
 import DestinationDetails from './DestinationDetails.jsx';
 require('./JourneyPlan.scss');
 
@@ -26,45 +27,52 @@ let JourneyPlan = React.createClass({
       <div className={classSet(journeyPlanClasses)}>
       	<div className="JourneyPlanTitle">MY JOURNEY</div>
       	<ul className="JourneyPlan__items">
-          {_.map(this.props.flights, flight => {
-            let date = moment(flight.departure).date();
-            let month = moment(flight.departure).format('MMM');
-            let daysStaying;
-            if (flight.daysStaying) {
-              daysStaying = _.initial(flight.daysStaying.split(' ')).join(' ');
-            }
-            return (
-              <span>
-                <li className="JourneyPlan__items__flight">
-                  <div className="JourneyPlan__items__flight__date">
-                    <div className="JourneyPlan__items__flight__date__day">{date}</div>
-                    <div className="JourneyPlan__items__flight__date__month">{month}</div>
-                  </div>
-                  <div className="JourneyPlan__items__flight__details">
-                    <div className="JourneyPlan__items__flight__from">
-                      <span className="JourneyPlan__items__flight__from__airportName">{flight.departureCountry.airportName}</span>
-                      <span className="JourneyPlan__items__flight__to__airportCode">({flight.departureCountry.airportCode})</span>
+          <TimeoutTransitionGroup
+          enterTimeout={800}
+          leaveTimeout={200}
+          transitionName="collapse"
+          >
+            {_.map(this.props.flights, flight => {
+              let date = moment(flight.departure).date();
+              let month = moment(flight.departure).format('MMM');
+              let daysStaying;
+              if (flight.daysStaying) {
+                daysStaying = _.initial(flight.daysStaying.split(' ')).join(' ');
+              }
+              return (
+                <span className="JourneyPlan__items__item">
+                  <li className="JourneyPlan__items__flight">
+                    <div className="JourneyPlan__items__flight__date">
+                      <div className="JourneyPlan__items__flight__date__day">{date}</div>
+                      <div className="JourneyPlan__items__flight__date__month">{month}</div>
                     </div>
-                    <div className="JourneyPlan__items__flight__plane"></div>
-                    <div className="JourneyPlan__items__flight__to">
-                      <span className="JourneyPlan__items__flight__to__airportName">{flight.arrivalCountry.airportName}</span>
-                      <span className="JourneyPlan__items__flight__to__airportCode">({flight.arrivalCountry.airportCode})</span>
+                    <div className="JourneyPlan__items__flight__details">
+                      <div className="JourneyPlan__items__flight__from">
+                        <span className="JourneyPlan__items__flight__from__airportName">{flight.departureCountry.airportName}</span>
+                        <span className="JourneyPlan__items__flight__to__airportCode">({flight.departureCountry.airportCode})</span>
+                      </div>
+                      <div className="JourneyPlan__items__flight__plane"></div>
+                      <div className="JourneyPlan__items__flight__to">
+                        <span className="JourneyPlan__items__flight__to__airportName">{flight.arrivalCountry.airportName}</span>
+                        <span className="JourneyPlan__items__flight__to__airportCode">({flight.arrivalCountry.airportCode})</span>
+                      </div>
+                      <div className="JourneyPlan__items__flight__price">${Math.round(flight.price)}</div>
                     </div>
-                    <div className="JourneyPlan__items__flight__price">${Math.round(flight.price)}</div>
-                  </div>
-                </li>
-                <DestinationDetails 
-                  daysStaying={daysStaying}
-                  flight={flight}
-                  flux={this.props.flux}
-                />
-              </span>
-            );
-          })}
+                  </li>
+                  <DestinationDetails 
+                    daysStaying={daysStaying}
+                    flight={flight}
+                    flux={this.props.flux}
+                  />
+                </span>
+              );
+            })}
+          </TimeoutTransitionGroup>
       	</ul>
       </div>
     );
-  },
+  }
 });
+
 
 export default JourneyPlan;
